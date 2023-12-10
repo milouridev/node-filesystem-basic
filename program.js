a1b7f3bccc05177e17006f70699b463bf4ce31d4
Camilo Uribe <milouridev@gmail.com>
2023-12-10 15:36:30 -0500

    Escritura de archivos y node con módulos de ES6

---
+++ program.js
@@ -0,0 +1,8 @@
+import saveFile from "./saveFile";
+
+const main = () => {
+  console.log("Program started");
+  saveFile("example.txt", "Example content");
+};
+
+main();
---
+++ saveFile.js
@@ -0,0 +1,27 @@
+import { createRequire } from "module";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const require = createRequire(import.meta.url);
+const fs = require("fs");
+const path = require("path");
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const logModuleDetails = () => {
+  console.log(`Module imported from ${__filename}`);
+  console.log(`Module located in directory: ${__dirname}`);
+};
+
+const saveFile = (filename, content, directory = "out") => {
+  const filePath = path.join(__dirname, directory, filename);
+  fs.mkdirSync(path.dirname(filePath), { recursive: true });
+  fs.writeFileSync(filePath, content);
+
+  console.log(`File saved successfully at ${filePath}`);
+};
+
+logModuleDetails();
+
+export default saveFile;
